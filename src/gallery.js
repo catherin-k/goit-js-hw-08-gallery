600f6768ea0acbb97004e79d2e6b641d6a3b12ab
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2020-11-21 20:05:13 +0200

    add modal close & clean src

--- src/gallery.js
+++ src/gallery.js
@@ -4,15 +4,17 @@ const refs = {
   gallery: document.querySelector(".js-gallery"),
   lightbox: document.querySelector(".js-lightbox"),
   lightbox__img: document.querySelector(".lightbox__image"),
+  lightbox__button: document.querySelector(".lightbox__button"),
+  overlay: document.querySelector(".lightbox__overlay"),
 };
-
+console.log(refs.overlay);
 // Створення розмітки по шаблону
 
 const makeGalleryItem = ({ preview, original, description }) =>
-  `<li class="gallery__item"> 
-    <a class="gallery__link" href=${original}> 
+  `<li class="gallery__item">
+    <a class="gallery__link" href=${original}>
   <img class="gallery__image"
-  src=  ${preview} data-source=${original} 
+  src=  ${preview} data-source=${original}
   alt=${description} /></a></li>`;
 
 const galleryListMarkup = images.reduce(
@@ -24,6 +26,8 @@ refs.gallery.innerHTML = galleryListMarkup;
 // Реалізація делегування на галереї ul.js-gallery і отримання url великого зображення.
 
 refs.gallery.addEventListener("click", onGalleryClick);
+refs.lightbox__button.addEventListener("click", onModalClose);
+refs.overlay.addEventListener("click", onModalClose);
 
 function onGalleryClick(event) {
   event.preventDefault();
@@ -34,5 +38,10 @@ function onGalleryClick(event) {
   // відкриття модального вікна
   refs.lightbox.classList.add("is-open");
   refs.lightbox__img.src = event.target.getAttribute("data-source");
+}
+
+function onModalClose(event) {
+  refs.lightbox__img.src = "";
+  refs.lightbox.classList.remove("is-open");
 }
 console.dir(onGalleryClick);
